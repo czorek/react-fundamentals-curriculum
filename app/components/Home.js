8ed00fc5334d27e011a7da29da645d88055b11a6
Radosław Wieczorek <radoslaw.wieczorek24@gmail.com>
2016-06-23 16:28:33 +0200

    Add home indexRoute

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,9 +3,7 @@ import React from 'react'
 export default React.createClass({
   render() {
     return (
-      <div className='mainContainer'>
-        {this.props.children}
-      </div>
+      <div>Hello from Home</div>
     );
   }
 })
